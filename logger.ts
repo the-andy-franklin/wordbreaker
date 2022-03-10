2184c962bd0f4127c9cf7c8104d6043aefbe2647
theandrewfranklin <theandrewfranklin@gmail.com>
2022-03-10 08:55:06 -0800

    logger, debugger, and prettier configured

---
+++ logger.ts
@@ -0,0 +1,19 @@
+const chalk = require("chalk");
+["debug", "log", "warn", "error"].forEach((methodName) => {
+  const originalMethod = console[methodName];
+  console[methodName] = (...args) => {
+    try {
+      throw new Error();
+    } catch (error) {
+      const trace = error.stack // grab the stack trace
+        .split("\n")[2] // slice off first 2 lines
+        .trim() // remove excess space
+        .substring(3) // remove first 3 characters ("at ")
+        .replace(`${process.cwd()}/`, "") // remove script folder path
+        .replace(/\s\(/, " at ") // removes the open parens and replaces it with " at "
+        .replace(/\)/, ""); // removes close parens
+
+      originalMethod.apply(console, [chalk.green(trace), "\n", ...args]);
+    }
+  };
+});
--- wordbreaker.ts
+++ wordbreaker.ts
@@ -1,77 +1,78 @@
-const fs = require("fs");
-const argv = require("yargs")(process.argv.slice(2)).argv;
+require('./logger');
+const fs = require('fs');
+const argv = require('yargs')(process.argv.slice(2)).argv;
 
 const wordCost: { [key: string]: number } = {};
 let maxWordLen = 0;
 
-const data = fs.readFileSync("./wordlist.txt", { encoding: "utf-8" });
+const data = fs.readFileSync('./wordlist.txt', { encoding: 'utf-8' });
 
-const words: string[] = data.split("\n");
+const words: string[] = data.split('\n');
 words.forEach((word, index) => {
-  wordCost[word] = Math.log((index + 1) * Math.log(words.length));
-  if (word.length > maxWordLen) {
-    maxWordLen = word.length;
-  }
+	wordCost[word] = Math.log((index + 1) * Math.log(words.length));
+	if (word.length > maxWordLen) {
+		maxWordLen = word.length;
+	}
 });
 
 argv._.forEach((arg: string) => {
-  console.log(breakwords(arg).join(" "));
+	console.log(breakwords(arg).join(' '));
 });
 
 function breakwords(str: string): string[] {
-  const list: string[] = [];
-  str.split(/\b(?=[^\w'])|(?<=[^\w'])\b/g).forEach((substring: string) => {
-    if (substring.match(/[^\w']/)) {
-      list.push(substring);
-    } else {
-      _split(substring).forEach((word: string) => {
-        list.push(word);
-      });
-    }
-  });
-  return list;
+	const list: string[] = [];
+	str.split(/\b(?=[^\w'])|(?<=[^\w'])\b/g).forEach((substring: string) => {
+		if (substring.match(/[^\w']/)) {
+			list.push(substring);
+		} else {
+			_split(substring).forEach((word: string) => {
+				list.push(word);
+			});
+		}
+	});
+	return list;
 }
 
 function _split(s: string): string[] {
-  const cost = [0];
+	const cost = [0];
 
-  function best_match(i: number): number[] {
-    const candidates = cost.slice(Math.max(0, i - maxWordLen), i).reverse();
-    let minPair = [Number.MAX_SAFE_INTEGER, 0];
-    candidates.forEach((c: number, k: number) => {
-      if (wordCost[s.substring(i - k - 1, i).toLowerCase()]) {
-        const ccost = c + wordCost[s.substring(i - k - 1, i).toLowerCase()];
-        if (ccost < minPair[0]) {
-          minPair = [ccost, k + 1];
-        }
-      }
-    });
-    return minPair;
-  }
+	function best_match(i: number): number[] {
+		const candidates = cost.slice(Math.max(0, i - maxWordLen), i).reverse();
+		let minPair = [Number.MAX_SAFE_INTEGER, 0];
+		candidates.forEach((c: number, k: number) => {
+			if (wordCost[s.substring(i - k - 1, i).toLowerCase()]) {
+				const ccost = c + wordCost[s.substring(i - k - 1, i).toLowerCase()];
+				if (ccost < minPair[0]) {
+					minPair = [ccost, k + 1];
+				}
+			}
+		});
+		return minPair;
+	}
 
-  for (let i = 1; i < s.length + 1; i++) {
-    cost.push(best_match(i)[0]);
-  }
+	for (let i = 1; i < s.length + 1; i++) {
+		cost.push(best_match(i)[0]);
+	}
 
-  const out: string[] = [];
-  for (let i = s.length; i > 0; i -= best_match(i)[1]) {
-    const [c, k] = best_match(i);
+	const out: string[] = [];
+	for (let i = s.length; i > 0; i -= best_match(i)[1]) {
+		const [c, k] = best_match(i);
 
-    if (out.length > 0) {
-      const outTail = out.slice(-1)[0];
-      if (
-        outTail.match(/^'s$/) ||
-        (outTail.match(/^\d+('s)?$/) && s[i - 1].match(/^\d$/))
-      ) {
-        out.pop();
-        out.push(s.slice(i - k, i) + outTail);
-      } else {
-        out.push(s.slice(i - k, i));
-      }
-    } else {
-      out.push(s.slice(i - k, i));
-    }
-  }
+		if (out.length > 0) {
+			const outTail = out.slice(-1)[0];
+			if (
+				outTail.match(/^'s$/) ||
+				(outTail.match(/^\d+('s)?$/) && s[i - 1].match(/^\d$/))
+			) {
+				out.pop();
+				out.push(s.slice(i - k, i) + outTail);
+			} else {
+				out.push(s.slice(i - k, i));
+			}
+		} else {
+			out.push(s.slice(i - k, i));
+		}
+	}
 
-  return out.reverse();
+	return out.reverse();
 }
